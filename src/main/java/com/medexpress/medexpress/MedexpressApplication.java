f70ce359909898300f359635ab4466d8b6e9eef9
Giovanni Mirulla <mirullagiovanni@gmail.com>
2025-01-29 16:54:11 +0100

    Init commit

---
+++ src/main/java/com/medexpress/medexpress/MedexpressApplication.java
@@ -0,0 +1,13 @@
+package com.medexpress.medexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedexpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medexpress/medexpress/MedexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medexpress.medexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

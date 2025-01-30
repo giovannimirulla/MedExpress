f5ef51db0f635695b2bc472eceb591a32ecef287
Giovanni Mirulla <mirullagiovanni@gmail.com>
2025-01-30 19:29:33 +0100

    Frontend creation

--- src/main/java/com/medexpress/medexpress/MedexpressApplication.java
+++ src/main/java/com/medexpress/medexpress/MedexpressApplication.java
@@ -2,12 +2,14 @@ package com.medexpress.medexpress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 
 @SpringBootApplication
-public class MedexpressApplication {
+@Theme("my-theme")
+public class MedexpressApplication implements AppShellConfigurator {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MedexpressApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MedexpressApplication.class, args);
+    }
 }

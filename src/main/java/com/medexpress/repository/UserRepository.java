e6cb24fff8314d614bf6769139e6d57f4b364ab6
Mariachiara <mariachiara.mo@gmail.com>
2025-02-06 14:53:29 +0100

    uc22

--- src/main/java/com/medexpress/repository/UserRepository.java
+++ src/main/java/com/medexpress/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.medexpress.repository;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import com.medexpress.entity.User;
@@ -13,4 +15,5 @@ public interface UserRepository extends MongoRepository <User, ObjectId> {
     public List<User> findAllByRole(Number role);
     public boolean existsByEmail(String email);
     public boolean existsByFiscalCode(String fiscalCode);
+    Optional<User> findByEmail(String email);
 }

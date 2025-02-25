362c5ae7ce84e6390606d3dcb77b728adf690ccb
Agata Rosselli <79996103+agatarosselli@users.noreply.github.com>
2025-02-26 00:59:08 +0100

    Added API to approve and reject request prescription of drugs

--- src/main/java/com/medexpress/controller/DoctorController.java
+++ src/main/java/com/medexpress/controller/DoctorController.java
@@ -10,9 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.HttpStatus;
 
+import com.medexpress.entity.Order;
 import com.medexpress.entity.User;
 
 import com.medexpress.service.UserService;
+import com.medexpress.service.OrderService;
 
 @RestController
 @RequestMapping("/api/v1/doctor")
@@ -21,13 +23,30 @@ public class DoctorController {
     @Autowired
     private UserService userService;
 
-    public DoctorController(UserService userService) {
+    @Autowired
+    private OrderService orderService;
+
+    // constructor
+    public DoctorController(UserService userService, OrderService orderService) {
         this.userService = userService;
+        this.orderService = orderService;
     }
 
     // search doctor by name or lastname using one query string
     @RequestMapping("/search")
     public ResponseEntity<List<User>> searchDoctor(@RequestParam String query) {
         return new ResponseEntity<List<User>>(userService.searchDoctor(query), HttpStatus.OK);
+    }
+
+    // approve prescription request of a drug in an order
+    @RequestMapping("/approve")
+    public ResponseEntity<Order> approvePrescription(@RequestParam String orderId) {
+        return new ResponseEntity<Order>(orderService.approvePrescription(orderId), HttpStatus.OK);
+    }
+
+    // reject prescription request of a drug in an order
+    @RequestMapping("/reject")
+    public ResponseEntity<Order> rejectPrescription(@RequestParam String orderId) {
+        return new ResponseEntity<Order>(orderService.rejectPrescription(orderId), HttpStatus.OK);
     }
 }
--- src/main/java/com/medexpress/service/OrderService.java
+++ src/main/java/com/medexpress/service/OrderService.java
@@ -22,20 +22,27 @@ public class OrderService {
     @Autowired
     private UserRepository userRepository;
 
+    // create order
     public Order createOrder(String packageId, String userId, String drugId) {
 
         // check if user exists
         User user = userRepository.findById(new ObjectId(userId)).orElseThrow(() -> new RuntimeException("User not found"));
-
-       
-        
       
         Order order = new Order(packageId, user, null, null, drugId, LocalDateTime.now(), LocalDateTime.now(),  Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.PENDING, Order.Priority.LOW);
         return orderRepository.save(order);
-
-
     }
 
-
+    // approve prescription request of a drug in an order
+    public Order approvePrescription(String orderId) {
+        Order order = orderRepository.findById(new ObjectId(orderId)).orElseThrow(() -> new RuntimeException("Order not found!"));
+        order.setStatusDoctor(Order.StatusDoctor.APPROVED);
+        return orderRepository.save(order);
+    }
     
+    // reject prescription request of a drug in an order
+    public Order rejectPrescription(String orderId) {
+        Order order = orderRepository.findById(new ObjectId(orderId)).orElseThrow(() -> new RuntimeException("Order not found!"));
+        order.setStatusDoctor(Order.StatusDoctor.REJECTED);
+        return orderRepository.save(order);
+    }
 }

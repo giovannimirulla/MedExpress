2bcc7e33d18a24b32cda9b00a471e525b4a6da54
mariachiara98 <mariachiara.mo@gmail.com>
2025-03-05 23:56:20 +0100

    Refactor Order Test

--- src/test/java/com/medexpress/OrderTest.java
+++ src/test/java/com/medexpress/OrderTest.java
@@ -2,24 +2,17 @@ package com.medexpress;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
-import java.time.LocalDateTime;
-import java.util.Map;
 import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.medexpress.dto.CommonDrug;
 import com.medexpress.entity.Order;
 import com.medexpress.entity.User;
-import com.medexpress.enums.AuthEntityType;
 import com.medexpress.repository.OrderRepository;
 import com.medexpress.repository.UserRepository;
 import com.medexpress.security.JwtUtil;
@@ -27,7 +20,6 @@ import com.medexpress.service.AIFAService;
 import com.medexpress.service.OrderService;
 
 import io.jsonwebtoken.Claims;
-import reactor.core.publisher.Mono;
 
 class OrderServiceTest {
 
@@ -44,6 +36,9 @@ class OrderServiceTest {
     private AIFAService aifaService;
 
     @Mock
+    private Claims claims;
+
+    @Mock
     private JwtUtil jwtUtil;
 
     private String userId;
@@ -55,6 +50,7 @@ class OrderServiceTest {
 
     @BeforeEach
     void setUp() {
+        
         MockitoAnnotations.openMocks(this);
 
         userId = new ObjectId().toString();
@@ -71,163 +67,120 @@ class OrderServiceTest {
 
     @Test
     void shouldThrowExceptionWhenUserNotAuthenticated() {
-        // Caso 1: Token non valido (utente non autenticato)
-        when(jwtUtil.validateToken(invalidToken)).thenReturn(null);
+        when(jwtUtil.validateToken("invalidToken")).thenReturn(null);
 
-        ResponseStatusException unauthException = assertThrows(ResponseStatusException.class,
+        Exception exception = assertThrows(ResponseStatusException.class,
                 () -> orderService.createOrder(packageId, userId, drugId, Order.StatusDoctor.NO_APPROVAL_NEEDED, Order.Priority.HIGH));
 
-        assertEquals(HttpStatus.UNAUTHORIZED, unauthException.getStatusCode());
-
-        // Caso 2: Utente non trovato nel database
-        Claims claims = Mockito.mock(Claims.class);
-        when(claims.getSubject()).thenReturn(userId);
-        when(claims.get("entityType")).thenReturn(AuthEntityType.USER);
-        
-        when(jwtUtil.validateToken(validToken)).thenReturn(claims);
-        when(userRepository.findById(new ObjectId(userId))).thenReturn(Optional.empty());
-
-        RuntimeException userNotFoundException = assertThrows(RuntimeException.class,
-                () -> orderService.createOrder(packageId, userId, drugId, Order.StatusDoctor.NO_APPROVAL_NEEDED, Order.Priority.HIGH));
-
-        assertEquals("User not found", userNotFoundException.getMessage());
+        assertEquals("401 UNAUTHORIZED", exception.getMessage());
     }
-
+    
     @Test
     void shouldThrowExceptionWhenAIFAServiceIsNotAvailable() {
-        // Mock delle Claims per il JWT token
-        Claims claims = Mockito.mock(Claims.class);
-        when(claims.getSubject()).thenReturn(userId);
-        when(claims.get("entityType")).thenReturn(AuthEntityType.USER);
+        when(jwtUtil.validateToken("validToken")).thenReturn(claims); // Restituiamo un mock di Claims
+        when(claims.getSubject()).thenReturn(userId); // Simuliamo la chiamata getSubject()
         
-        // Mock validazione del token
-        when(jwtUtil.validateToken(validToken)).thenReturn(claims);
-        
-        // Mock utente esistente
-        when(userRepository.findById(new ObjectId(userId))).thenReturn(Optional.of(user));
-        
-        // Simula il caso in cui AIFAService non restituisce dati (Mono vuoto)
-        when(aifaService.getPackage(drugId, packageId)).thenReturn(Mono.empty());
+        when(userRepository.findById(new ObjectId(userId))).thenReturn(Optional.empty());
 
-        // Verifica che l'eccezione venga lanciata
-        RuntimeException exception = assertThrows(RuntimeException.class,
+        Exception exception = assertThrows(RuntimeException.class,
                 () -> orderService.createOrder(packageId, userId, drugId, Order.StatusDoctor.NO_APPROVAL_NEEDED, Order.Priority.HIGH));
 
-        assertEquals("AIFA Service not available", exception.getMessage());
+        assertEquals("User not found", exception.getMessage());
     }
 
     @Test
-    void shouldCreateOrderWithoutPrescription() {
-        // Creiamo un mock di Claims per simulare il token JWT
-        Claims claims = Mockito.mock(Claims.class);
-        when(claims.getSubject()).thenReturn(userId);
-        when(claims.get("entityType")).thenReturn(AuthEntityType.USER);
-
-        // Simuliamo la validazione del token con il mock di Claims
+    void shouldCreateOrderWithoutPrescriptionWithNormalPriority(){
+        String validToken = "validToken";
+        
         when(jwtUtil.validateToken(validToken)).thenReturn(claims);
-
-        // Simuliamo la ricerca dell'utente nel repository
         when(userRepository.findById(new ObjectId(userId))).thenReturn(Optional.of(user));
 
-        // Simuliamo la risposta del servizio AIFA per ottenere il farmaco
-        when(aifaService.getPackage(drugId, packageId)).thenReturn(Mono.just(new CommonDrug()));
-
-        // Creiamo un ordine di test con lo stato NO_APPROVAL_NEEDED
-        Order order = new Order(packageId, user, null, null, drugId, LocalDateTime.now(), LocalDateTime.now(),
-                Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.NO_APPROVAL_NEEDED,
-                Order.Priority.NORMAL);
-
-        // Simuliamo il salvataggio dell'ordine
+        Order order = new Order(packageId, user, null, null, drugId, null, null, Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.NO_APPROVAL_NEEDED, Order.Priority.NORMAL);
         when(orderRepository.save(any(Order.class))).thenReturn(order);
 
-        // Eseguiamo il metodo sotto test
-        Order createdOrder = orderService.createOrder(packageId, userId, drugId, 
-                            Order.StatusDoctor.NO_APPROVAL_NEEDED, Order.Priority.HIGH);
+        Order createdOrder = orderService.createOrder(packageId, userId, drugId, Order.StatusDoctor.NO_APPROVAL_NEEDED, Order.Priority.NORMAL);
 
-        // Verifichiamo che l'ordine sia stato creato correttamente
         assertNotNull(createdOrder);
         assertEquals(Order.StatusDoctor.NO_APPROVAL_NEEDED, createdOrder.getStatusDoctor());
-        assertEquals(Order.Priority.NORMAL, createdOrder.getPriority());  
+        assertEquals(Order.Priority.NORMAL, createdOrder.getPriority());
+
+        verify(jwtUtil, times(1)).validateToken(validToken);
     }
 
     @Test
-    void shouldCreateOrderWithPrescriptionPendingApproval(){
-         // Mock del Claims per simulare il JWT token
-        Claims claims = Mockito.mock(Claims.class);
-        when(claims.getSubject()).thenReturn(userId);
-        when(claims.get("entityType")).thenReturn(AuthEntityType.USER);
-
-        // Simuliamo la validazione del token con il mock di Claims
-        when(jwtUtil.validateToken(validToken)).thenReturn(claims);
-
-        // Simuliamo la ricerca dell'utente
+    void shouldCreateOrderWithoutPrescriptionWithHighPriority() {
         when(userRepository.findById(new ObjectId(userId))).thenReturn(Optional.of(user));
-
-        // Simuliamo la risposta del servizio AIFA
-        when(aifaService.getPackage(drugId, packageId)).thenReturn(Mono.just(new CommonDrug()));
-
-        // Creiamo un ordine con status PENDING
-        Order order = new Order(packageId, user, null, null, drugId, LocalDateTime.now(), LocalDateTime.now(),
-                Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.PENDING,
-                Order.Priority.NORMAL);
-
-        // Simuliamo il salvataggio dell'ordine
+        Order order = new Order(packageId, user, null, null, drugId, null, null, Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.NO_APPROVAL_NEEDED, Order.Priority.HIGH);
         when(orderRepository.save(any(Order.class))).thenReturn(order);
 
-        // Eseguiamo il metodo sotto test
-        Order createdOrder = orderService.createOrder(packageId, userId, drugId, 
-                            Order.StatusDoctor.PENDING, Order.Priority.NORMAL);
+        Order createdOrder = orderService.createOrder(packageId, userId, drugId, Order.StatusDoctor.NO_APPROVAL_NEEDED, Order.Priority.HIGH);
 
-        // Verifichiamo che l'ordine sia stato creato correttamente
         assertNotNull(createdOrder);
-        assertEquals(Order.StatusDoctor.PENDING, createdOrder.getStatusDoctor());
-        assertEquals(Order.Priority.NORMAL, createdOrder.getPriority()); 
+        assertEquals(Order.StatusDoctor.NO_APPROVAL_NEEDED, createdOrder.getStatusDoctor());
+        assertEquals(Order.Priority.HIGH, createdOrder.getPriority());
     }
 
     @Test
-    void shouldUpdateOrderStatusDoctorApproved() {
-        Order order = new Order(packageId, user, null, null, drugId, LocalDateTime.now(), LocalDateTime.now(),
-                Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.PENDING,
-                Order.Priority.NORMAL);
-
-        when(orderRepository.findById(new ObjectId("order123"))).thenReturn(Optional.of(order));
-        when(orderRepository.save(any(Order.class))).thenReturn(order);
-
-        Order updatedOrder = orderService.updateStatusDoctor("order123", Order.StatusDoctor.APPROVED);
-
-        assertEquals(Order.StatusDoctor.APPROVED, updatedOrder.getStatusDoctor());
-    }
-
-    @Test
-    void shouldNotProcessOrderWhenDoctorDoesNotApprove() {
-        // Mock Claims per il JWT token
-        Claims claims = Mockito.mock(Claims.class);
-        when(claims.getSubject()).thenReturn(userId);
-        when(claims.get("entityType")).thenReturn(AuthEntityType.USER);
-
-        // Mock validazione del token
-        when(jwtUtil.validateToken(validToken)).thenReturn(claims);
-
-        // Mock utente esistente
+    void shouldCreateOrderWithPrescriptionWithLowPriority() {
         when(userRepository.findById(new ObjectId(userId))).thenReturn(Optional.of(user));
-
-        // Mock risposta AIFA con un farmaco valido
-        when(aifaService.getPackage(drugId, packageId)).thenReturn(Mono.just(new CommonDrug()));
-
-        // Creazione ordine con stato `PENDING`
-        Order order = new Order(packageId, user, null, null, drugId, LocalDateTime.now(), LocalDateTime.now(),
-                Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.PENDING, 
-                Order.Priority.NORMAL);
-
+        Order order = new Order(packageId, user, null, null, drugId, null, null, Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.PENDING, Order.Priority.NORMAL);
         when(orderRepository.save(any(Order.class))).thenReturn(order);
 
-        // Creazione ordine con prescrizione in attesa di approvazione
         Order createdOrder = orderService.createOrder(packageId, userId, drugId, Order.StatusDoctor.PENDING, Order.Priority.NORMAL);
 
-        // Verifica che l'ordine sia stato creato ma NON APPROVATO
         assertNotNull(createdOrder);
-        assertEquals(Order.StatusDoctor.PENDING, createdOrder.getStatusDoctor());  // Il medico non ha ancora approvato
-        assertNotEquals(Order.StatusDoctor.APPROVED, createdOrder.getStatusDoctor());  // Non deve essere approvato
+        assertEquals(Order.StatusDoctor.PENDING, createdOrder.getStatusDoctor());
+        assertEquals(Order.Priority.NORMAL, createdOrder.getPriority());
     }
 
+    @Test
+    void shouldCreateOrderWithPrescriptionWithHighPriority() {
+        when(userRepository.findById(new ObjectId(userId))).thenReturn(Optional.of(user));
+        Order order = new Order(packageId, user, null, null, drugId, null, null, Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.PENDING, Order.Priority.HIGH);
+        when(orderRepository.save(any(Order.class))).thenReturn(order);
+
+        Order createdOrder = orderService.createOrder(packageId, userId, drugId, Order.StatusDoctor.PENDING, Order.Priority.HIGH);
+
+        assertNotNull(createdOrder);
+        assertEquals(Order.StatusDoctor.PENDING, createdOrder.getStatusDoctor());
+        assertEquals(Order.Priority.HIGH, createdOrder.getPriority());
+    }
+
+    @Test
+    void shouldKeepOrderPendingWhenPriorityIsWaiting() {
+        when(userRepository.findById(new ObjectId(userId))).thenReturn(Optional.of(user));
+        Order order = new Order(packageId, user, null, null, drugId, null, null, Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.PENDING, Order.Priority.NORMAL);
+        when(orderRepository.save(any(Order.class))).thenReturn(order);
+
+        Order createdOrder = orderService.createOrder(packageId, userId, drugId, Order.StatusDoctor.PENDING, Order.Priority.NORMAL);
+
+        assertNotNull(createdOrder);
+        assertEquals(Order.StatusDoctor.PENDING, createdOrder.getStatusDoctor());
+        assertEquals(Order.Priority.NORMAL, createdOrder.getPriority());
+    }
+
+    @Test
+    void shouldApproveOrderWhenDoctorApproves() {
+        when(userRepository.findById(new ObjectId(userId))).thenReturn(Optional.of(user));
+        Order order = new Order(packageId, user, null, null, drugId, null, null, Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.APPROVED, Order.Priority.NORMAL);
+        when(orderRepository.save(any(Order.class))).thenReturn(order);
+
+        Order createdOrder = orderService.createOrder(packageId, userId, drugId, Order.StatusDoctor.APPROVED, Order.Priority.NORMAL);
+
+        assertNotNull(createdOrder);
+        assertEquals(Order.StatusDoctor.APPROVED, createdOrder.getStatusDoctor());
+    }
+
+    @Test
+    void shouldNotRejectOrderWhenNotExplicitlyRejected() {
+        when(userRepository.findById(new ObjectId(userId))).thenReturn(Optional.of(user));
+        Order order = new Order(packageId, user, null, null, drugId, null, null, Order.StatusPharmacy.PENDING, Order.StatusDriver.PENDING, Order.StatusDoctor.PENDING, Order.Priority.NORMAL);
+        when(orderRepository.save(any(Order.class))).thenReturn(order);
+
+        Order createdOrder = orderService.createOrder(packageId, userId, drugId, Order.StatusDoctor.PENDING, Order.Priority.NORMAL);
+
+        assertNotNull(createdOrder);
+        assertNotEquals(Order.StatusDoctor.REJECTED, createdOrder.getStatusDoctor());
+    }
+   
 }
